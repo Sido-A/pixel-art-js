3cf379bc84c9d870e5b15fcf837c1bc943076bbb
Sido-A <sidonie.abe.uk@gmail.com>
2020-07-04 19:35:26 +0100

    edit js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const pixelColors = document.querySelector(".pixel-colors");
 const pixelInnerBlocks = document.querySelector(".canvas-inner");
-const tds = pixelInnerBlocks.querySelectorAll("td");
+const canvasTds = pixelInnerBlocks.querySelectorAll("td");
 const colorArray = [];
 
 pixelColors.addEventListener("click", (e) => {
   const color = e.target.classList.value;
   if (colorArray === []) {
     colorArray.push(color);
+
   } else if (color === "clear-all") {
-    console.log("clear ALLLLL");
-    tds.forEach((td) => {
+    canvasTds.forEach((td) => {
       td.style.backgroundColor = "white";
     });
+
   } else {
     colorArray.pop();
     colorArray.push(color);
   }
 });
 
-tds.forEach((td) => {
+
+canvasTds.forEach((td) => {
   td.addEventListener("click", (e) => {
     const element = e.target;
     element.style.backgroundColor = colorArray;
